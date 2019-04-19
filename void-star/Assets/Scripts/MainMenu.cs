fc089ddd49e9c7a6872d454f59076fb5ab61344b
Elie Puffelis <eliepuffelis@gmail.com>
2019-04-19 09:14:34 -0700

    Setup build index hierarchy and created a load scene for battle scene

--- void-star/Assets/Scripts/MainMenu.cs
+++ void-star/Assets/Scripts/MainMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuButtons : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
    public void QuitButton()
     {
--- void-star/Assets/Scripts/VehicleSelection.cs
+++ void-star/Assets/Scripts/VehicleSelection.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class VehicleSelection : MonoBehaviour
 {
@@ -13,26 +14,23 @@ public class VehicleSelection : MonoBehaviour
     void Start()
     {
 
+        //initializing the vehicle models
         vehicles = new GameObject[3];
         vehicles[0] = GameObject.Find("TieFighter");
         vehicles[1] = GameObject.Find("TieSilencer");
         vehicles[2] = GameObject.Find("X-Wing");
-
+        //displaying only the first of the 3 vehicles
         vehicles[1].SetActive(false);
         vehicles[2].SetActive(false);
 
+        //initializing the background images
         vehicleBackgroundImages = new GameObject[3];
         vehicleBackgroundImages[0] = GameObject.Find("TieFighterImage");
         vehicleBackgroundImages[1] = GameObject.Find("TieSilencerImage");
         vehicleBackgroundImages[2] = GameObject.Find("X-WingImage");
-
+        //displaying only the first of the 3 bgimages corresponding to its matching vehicle
         vehicleBackgroundImages[1].SetActive(false);
         vehicleBackgroundImages[2].SetActive(false);
-
-
-
-
-
     }
 
 
@@ -44,18 +42,21 @@ public class VehicleSelection : MonoBehaviour
         else
             showIndex--;
 
+        PlayVehicleSelectionButtonSfx();
         DisplayVehicle();
         DisplayBgImage();
     }
 
     public void NextVehicle()
     {
+
         hideIndex = showIndex;
         if (showIndex == 2)
             showIndex = 0;
         else
             showIndex++;
 
+        PlayVehicleSelectionButtonSfx();
         DisplayVehicle();
         DisplayBgImage();
     }
@@ -73,6 +74,18 @@ public class VehicleSelection : MonoBehaviour
 
     public void SelectVehicle()
     {
+        PlayVehicleSelectionButtonSfx();
+        StartCoroutine(GoToBattleScene());
+    }
 
+    public void PlayVehicleSelectionButtonSfx()
+    {
+        gameObject.GetComponents<AudioSource>()[0].Play();
+    }
+
+    IEnumerator GoToBattleScene()
+    {
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene(2);
     }
 }
