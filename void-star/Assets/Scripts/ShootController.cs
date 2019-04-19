19c332452131b406b59f2d05dc4f1fdad9dcf43c
Xxy <wangyao4ina@gmail.com>
2019-04-19 14:46:51 -0700

    Move code of hidding mouse to Start()

--- void-star/Assets/Scripts/ShootController.cs
+++ void-star/Assets/Scripts/ShootController.cs
@@ -10,13 +10,12 @@ public class ShootController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Cursor.visible = false;
         Shoot();
 
     }
