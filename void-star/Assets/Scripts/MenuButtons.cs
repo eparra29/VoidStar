3f039224c0392c61a7f2b776ac253c0d9829c8fe
Elie Puffelis <eliepuffelis@gmail.com>
2019-04-19 03:26:11 -0700

    Created Menu Scene and added menu theme and button sfx

---
+++ void-star/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+   public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void CallPlayButton()
+    {
+        StartCoroutine(PlayButton());
+    }
+
+    IEnumerator PlayButton()
+    {
+        PlayMenuButtonSfx();
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene(1);
+    }
+
+    public void PlayMenuButtonSfx()
+    {
+        gameObject.GetComponents<AudioSource>()[0].Play();
+    }
+
+
+
+
+}
