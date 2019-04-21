e93857b5543d355226a8664fcefd376aaf2ce184
Clayton Matulich <claytonmatulich13@gmail.com>
2019-04-20 19:44:56 -0700

    Added High Score UI when time runs out

--- void-star/Assets/Scripts/UImanager.cs
+++ void-star/Assets/Scripts/UImanager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UImanager : MonoBehaviour
 {
@@ -11,9 +12,29 @@ public class UImanager : MonoBehaviour
     [SerializeField]
     public Text timeNumber;
 
+    [SerializeField]
+    public Text highScoreNumber;
+
+    [SerializeField]
+    public GameObject HighScoreUI;
+
+    [SerializeField]
+    public GameObject ScoreUI;
+
+    [SerializeField]
+    public GameObject TimeUI;
+
+    [SerializeField]
+    public GameObject cockPitImage;
+
+    [SerializeField]
+    public GameObject myCamera;
+
     public int score;
 
-    public float time = 60;
+    public float time = 5;
+
+    private int highScore;
     
     // Start is called before the first frame update
     void Start()
@@ -28,5 +49,25 @@ public class UImanager : MonoBehaviour
         scoreNumber.text = score.ToString();
         timeNumber.text = time.ToString("N0");
         time -= Time.deltaTime; 
+
+        //end and show high score
+        if(time <= 0)
+        {
+            ScoreUI.SetActive(false);
+            TimeUI.SetActive(false);
+            cockPitImage.SetActive(false);
+            myCamera.GetComponent<ShootController>().enabled = false;
+            myCamera.GetComponentInParent<MouseLookController>().enabled = false;
+            highScore = score;
+            highScoreNumber.text = highScore.ToString();
+            HighScoreUI.SetActive(true);
+            StartCoroutine(LoadCredits());
+        }
+    }
+
+    IEnumerator LoadCredits()
+    {
+        yield return new WaitForSeconds(8.0f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
